a0cb5369d8bfecfec3f15e624102711e34898dba
Frederick Giasson <fred@fgiasson.com>
2023-10-15 13:23:25 -0400

    Adding folder monitoring for files changes

--- weborg/main.py
+++ weborg/main.py
@@ -1,9 +1,14 @@
 import os
+import time
 import typer
 from . import __version__
 from .container import *
+from .monitor import OrgFileChangeHandler
 from .org import tangle as org_tangle, detangle as org_detangle
 from rich import print
+from typing import List
+from typing_extensions import Annotated
+from watchdog.observers import Observer
 
 app = typer.Typer()
 
@@ -17,17 +22,41 @@ def config():
     """Get the current configuration of weborg"""
     # print(f"DOCS_PATH: {os.environ.get('DOCS_PATH')}")
 
+
+# Example: weborg tangle 'org/' --files=test.org --files=foo.org
 @app.command()
-def tangle(folder: str = typer.Argument(..., help="The folder where the Org-mode file to tangle are located")):
+def tangle(folder: str = typer.Argument(..., help="The folder where the Org-mode files to tangle are located"),
+           file: Annotated[List[str], 
+                           typer.Option("--file",
+                           help="Optional list of one or more files to tangle from `folder`")] = None):
     """Tangle the org files in the given folder"""
     folder = os.path.abspath(folder)
-    org_tangle(folder)
+    org_tangle(folder, file)
 
 @app.command()
-def detangle(folder: str = typer.Argument(..., help="The folder where the source files to detangle are located")):
+def detangle(folder: str = typer.Argument(..., help="The folder where the source files to detangle are located"),
+             file: Annotated[List[str], 
+                           typer.Option("--file",
+                           help="Optional list of one or more files to tangle from `folder`")] = None):
+                           
     """Detangle the source files in the given folder"""
     folder = os.path.abspath(folder)
-    org_detangle(folder)
+    org_detangle(folder, file)
+
+@app.command()
+def monitor(folder: str = typer.Argument(..., help="The folder to monitor for changes")):
+    """Monitor the given folder for changes and tangle the org files when they change"""
+    folder = os.path.abspath(folder)
+    event_handler = OrgFileChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=folder, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
 
 def init():
     """Initialize weborg"""
--- weborg/org.py
+++ weborg/org.py
@@ -1,20 +1,26 @@
 import os
 from weborg.container import container_exists, create_container, delete_container
 
-def tangle(folder: str) -> None:
+def tangle(folder: str, files: list=None) -> None:
     """Every time we tangle, we create a new container and attach 
     a volume to it from the provided folder, tangle and delete
-    it in case the next tangle uses a different folder"""
+    it in case the next tangle uses a different folder.
+    If `files` has a reference to one or more files, only
+    tangle the files, in the `folder`, that are in the `files` 
+    list."""
 
     try:
         volume = {f"{folder}": {'bind': '/mnt/org', 'mode': 'rw'}}
         container = create_container(volume)
 
-        print(f"Tangling {folder}...")
+        if not files:
+            print(f"Tangling {folder}...")
+        else:
+            print(f"Tangling {files}...")
 
         # for each org file in the folder, tangle it
         for file in os.listdir(folder):
-            if file.endswith(".org"):
+            if file.endswith(".org") and (not files or file in files):
                 print("Tangling:", file)
                 response = container.exec_run(f"emacs --batch --eval \"(progn (setq python-indent-guess-indent-offset t) (setq python-indent-guess-indent-offset-verbose nil) (find-file \\\"/mnt/org/{file}\\\") (org-babel-tangle))\"")
                 print(response.output.decode('utf-8'))
@@ -23,22 +29,28 @@ def tangle(folder: str) -> None:
     except Exception as e:
         print("Tangling canceled:", str(e))
     
-def detangle(folder: str) -> None:
+def detangle(folder: str, files: list=None) -> None:
     """Syhnchronize the source files there have been tangled back to their
     original Org code blocks. Code blocks needs to have the header 
     `:comments link` or `:comments both` to be detangled. If you use
     `:noweb yes` references, then the noweb references won't be detangled,
     and the original Org file will be missing the noweb references. So,
-    don't use detangle until detangling with noweb is fixed in Org-mode."""
+    don't use detangle until detangling with noweb is fixed in Org-mode.
+    If `files` has a reference to one or more files, only
+    tangle the files, in the `folder`, that are in the `files` 
+    list."""
     try:
         volume = {f"{folder}": {'bind': '/mnt/org', 'mode': 'rw'}}
         container = create_container(volume)
 
-        print(f"Detangling {folder}...")
+        if not files:
+            print(f"Detangling {folder}...")
+        else:
+            print(f"Detangling {files}...")
 
         # for each source file in the folder, detangle it
         for file in os.listdir(folder):
-            if not file.endswith(".org"):
+            if not file.endswith(".org") and (not files or file in files):
                 org_file = file.split(".")[0] + ".org"
                 print(f"Detangling: {file} into {org_file}")
                 response = container.exec_run(f"emacs --batch --eval \"(progn (require 'org) (setq make-backup-files nil) (setq python-indent-guess-indent-offset t) (setq python-indent-guess-indent-offset-verbose nil) (org-babel-detangle \\\"/mnt/org/{file}\\\") (switch-to-buffer \\\"{org_file}\\\") (save-buffer))\"")
